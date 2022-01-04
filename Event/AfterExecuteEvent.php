74f29deaf012a3e310c3c3c44a7e5af7d1e6b195
arikaim <info@arikaim.com>
2022-01-04 13:08:21 -0500

    =

--- Event/AfterExecuteEvent.php
+++ Event/AfterExecuteEvent.php
@@ -9,27 +9,12 @@
  */
 namespace Arikaim\Core\Console\Event;
 
-use Symfony\Component\Console\Event\ConsoleCommandEvent;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Event\ConsoleEvent;
 
 /**
  * After comment execute console event
  */
-class AfterExecuteEvent extends ConsoleCommandEvent 
+class AfterExecuteEvent extends ConsoleEvent 
 {       
     const EVENT_NAME = 'after.execute.commmand';
-
-    /**
-     * Constructor
-     *
-     * @param Command|null $command
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     */
-    public function __construct(Command $command = null, InputInterface $input, OutputInterface $output)
-    {
-        parent::__construct($command,$input,$output);
-    }
 }
--- Event/BeforeExecuteEvent.php
+++ Event/BeforeExecuteEvent.php
@@ -9,27 +9,12 @@
  */
 namespace Arikaim\Core\Console\Event;
 
-use Symfony\Component\Console\Event\ConsoleCommandEvent;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Event\ConsoleEvent;
 
 /**
  * Before comment execute console event
  */
-class BeforeExecuteEvent extends ConsoleCommandEvent 
+class BeforeExecuteEvent extends ConsoleEvent 
 {       
-    const EVENT_NAME = 'before.execute.commmand';
-
-    /**
-     * Constructor
-     *
-     * @param Command|null $command
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     */
-    public function __construct(Command $command = null, InputInterface $input, OutputInterface $output)
-    {
-        parent::__construct($command,$input,$output);
-    }
+    const EVENT_NAME = 'before.execute.commmand';    
 }
